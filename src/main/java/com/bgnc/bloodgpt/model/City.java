19f75614268572831be073745e514c9d7208ec85
BGNC <bugra34055@icloud.com>
2024-11-30 04:53:42 +0300

    city model is created

---
+++ src/main/java/com/bgnc/bloodgpt/model/City.java
@@ -0,0 +1,29 @@
+package com.bgnc.bloodgpt.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Table(name = "cities")
+public class City {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String name;
+
+    @OneToMany(mappedBy = "city", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<District> districts = new ArrayList<>();
+}
