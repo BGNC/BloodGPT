4c59af4b4edc466f3a74aa2ce335eb9131edfaa0
BGNC <bugra34055@icloud.com>
2024-12-01 05:14:16 +0300

    created blood test for model and it has many apis it should managed a apis layer.

--- src/main/java/com/bgnc/bloodgpt/utils/BloodTestControllerApis.java
+++ src/main/java/com/bgnc/bloodgpt/utils/BloodTestControllerApis.java
@@ -6,8 +6,12 @@ public class BloodTestControllerApis {
     public static final String BLOOD_TEST = "/blood-tests";
     public static final String ANALYZE = "/analyze";
     public static final String FILE = "file";
+    public static final String UPLOAD = "/upload";
+    public static final String USER_TESTS = "/{userId}";
 
-    // Tam Yollar
+    // ALL PATHS
     public static final String BLOOD_TEST_CONTROLLER = API + BLOOD_TEST;
     public static final String ANALYZE_BLOOD_TEST = BLOOD_TEST_CONTROLLER + ANALYZE;
+    public static final String UPLOAD_BLOOD_TEST = BLOOD_TEST_CONTROLLER + UPLOAD;
+    public static final String GET_USER_BLOOD_TESTS = BLOOD_TEST_CONTROLLER + USER_TESTS;
 }
