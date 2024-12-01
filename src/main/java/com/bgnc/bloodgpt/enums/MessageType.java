ba634a77c98bf44708c1d0d769ae5a667c3fb7d7
BGNC <bugra34055@icloud.com>
2024-12-02 01:09:46 +0300

    new message type is added. Thrown exception. AND FLAG 2 PROGRAM IS RUNNING.

--- src/main/java/com/bgnc/bloodgpt/enums/MessageType.java
+++ src/main/java/com/bgnc/bloodgpt/enums/MessageType.java
@@ -6,7 +6,6 @@ import lombok.Getter;
 public enum MessageType {
 
     FILE_NOT_FOUND_EXCEPTION("999","File not found"),
-
     INVALID_FILE("404","Invalid file. Please upload a valid PDF."),
     PROCESSING_FILE_EXCEPTION("405","Error processing the PDF file"),
     NO_RECORD_EXISTS("1001", "The record is not found"),
@@ -16,7 +15,6 @@ public enum MessageType {
     REFRESH_TOKEN_IS_ALREADY_EXPIRED("1005", "Refresh token is already expired"),
     USERNAME_OR_PASSWORD_IS_INVALID("1006", "Username or password is invalid"),
     TOKEN_IS_EXPIRED("1007", "The token is expired"),
-    CURRENCY_RATE_IS_OCCURED("1008", "The currency rate is occurred"),
     CLIENT_ERROR("1009", "A client error occurred (4xx)"),
     SERVER_ERROR("1010", "A server error occurred (5xx)"),
     AUTHENTICATION_FAILED_TC_NUMBER("1111", "Authentication failed"),
